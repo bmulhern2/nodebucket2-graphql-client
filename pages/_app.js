b08963bf7c89988543033f1eddfebd6650450208
bmulhern2 <bmulhern09@gmail.com>
2021-08-01 07:47:02 -0400

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+let graphqlUri = 'https://nodebucket2-api.herokuapp.com/graphql';
+
+const client = new ApolloClient({ uri: graphqlUri });
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+  <ApolloProvider client={client}>
+    <div>
+      <Component {...pageProps} />
+    </div>
+  </ApolloProvider>
+  );
 }
-
-export default MyApp
