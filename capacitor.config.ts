91ccf1bc6cb6bf0a263475c3d0a3ee5a3a6757b2
bmulhern2 <bmulhern09@gmail.com>
2021-08-01 07:35:41 -0400

    deploy

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nodebucket2.app',
+  appName: 'nodebucket2',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
